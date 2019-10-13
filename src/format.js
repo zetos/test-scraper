d09ff5cee9e8d23c70796ad4f5a57443f8db220c
Zetos Lima <cesar_manoel@hotmail.com>
2019-10-12 21:45:18 -0300

    checkIfDate function to validade entry.

--- src/format.js
+++ src/format.js
@@ -2,6 +2,9 @@ const isNumber = n => /^-?[\d.]+(?:e-?\d+)?$/.test(n);
 
 const getDate = str => str.match(/(\d{1,4}([.\-/])\d{1,2}([.\-/])\d{1,4})/g)[0];
 
+const checkIfDate = str =>
+  /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i.test(str);
+
 const extractParams = str =>
   str
     .match(/\(([^)]+)\)/)[1]
@@ -10,4 +13,4 @@ const extractParams = str =>
 
 const fixSpace = str => str.replace(/&nbsp;/g, ' ').trim();
 
-module.exports = { isNumber, getDate, extractParams, fixSpace };
+module.exports = { isNumber, getDate, checkIfDate, extractParams, fixSpace };
